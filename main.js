4db72a0e02fa4a27a0c9e2403fee2ee452e5447a
Stephen Carney <stephencarney1990@gmail.com>
2015-08-09 19:25:51 -0700

    fixed DOM issues

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ var Game = function(question, answer) {
   this.play = function() {
     this.guess = prompt(question);
     if (this.guess === answer) {
-      message = 'Correct!';
+     var gameSays = document.getElementById('game-says');
+gameSays.innerHTML = 'Correct!';
     } else {
-      message = 'Incorrect!';
+      var gameSays = document.getElementById('game-says');
+gameSays.innerHTML = 'Incorrect!';
     }
   };
 };
@@ -19,9 +21,3 @@ var game2 = new Game('What is the capital of Washington? a, Ontario, b, New York
 
 game1.play();
 game2.play();
-
-var gameSays = document.getElementById('game-says');
-gameSays.innerHTML = message;
-
-// Idea; include multiple game instances so that users can play against friends to see who can get the best score. Highest to lowest score, along with the user's name will be displayed in list form, or in a table.
-// Also, change the answer input method from the user to radio buttons instead of alerts for a more appropriate looking multiple choice format. -->
